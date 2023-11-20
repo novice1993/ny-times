b87c2497dc2cb467c9bc657cddbc3dd632e33379
novice1993 <novice1993@gmail.com>
2023-11-20 22:45:08 +0900

    [Fix] newpaper 관련 에러 처리
    - newpaper 정보가 undefined일 경우 오류 발생하지 않도록 엣지케이스 처리

--- src/utils/aboutOrganizeData/transformRawData.ts
+++ src/utils/aboutOrganizeData/transformRawData.ts
@@ -6,7 +6,8 @@ export const transformRawData = (originData: any) => {
   const articleInfo = originData.map((article: any) => {
     const information = {
       headline: article.headline.main,
-      newspaper: article.source.replace("International", ""),
+      newspaper:
+        article.source !== undefined ? article.source.replace("International", "") : "Unknown",
       reporter: changeReporterFormat(article.byline.original),
       date: changeDateFormat(article.pub_date),
       url: article.web_url,
